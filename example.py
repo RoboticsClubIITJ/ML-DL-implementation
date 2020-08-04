545705a2d611e6ae1cfc24d0a18b0f3375b303da
Rohan Singh <rohansingh9001@gmail.com>
2020-08-05 03:02:33 +0530

    Added Stochastic Gradient Descent

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from models import LinearRegression
-from optimizers import GradientDescent
+from optimizers import SGD
 from loss_func import MeanSquaredError
 from utils import read_data
 
@@ -8,7 +8,7 @@ X, Y = read_data('dataset1.txt')
 
 model = LinearRegression()
 
-optimizer = GradientDescent(0.001, MeanSquaredError)
+optimizer = SGD(0.0001, MeanSquaredError)
 
 model.fit(X, Y, optimizer=optimizer, epochs=25)
 
--- optimizers.py
+++ optimizers.py
@@ -1,4 +1,5 @@
 from loss_func import MeanSquaredError
+import random
 
 
 class GradientDescent():
@@ -18,3 +19,25 @@ class GradientDescent():
 
     def iterate(self, X, Y, W):
         return W - self.learning_rate * self.loss_func.derivative(X, Y, W)
+
+
+class StochasticGradientDescent():
+
+    def __init__(self, learning_rate=0.01, loss_func=MeanSquaredError):
+        self.learning_rate = learning_rate
+        self.loss_func = loss_func
+
+    def iterate(self, X, Y, W):
+        M, N = X.shape
+        i = random.randint(0, M-1)
+        x, y = X[i, :], Y[:, i]
+        x.shape, y.shape = (1, N), (1, 1)
+        return W - self.learning_rate * self.loss_func.derivative(x, y, W)
+
+
+class SGD(StochasticGradientDescent):
+    '''
+    An abstract class to provide an alias to the
+    really long class name StochasticGradientDescent
+    '''
+    pass
