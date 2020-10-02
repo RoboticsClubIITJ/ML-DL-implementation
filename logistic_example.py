47b67f39e91c73aae905c37705b1a7f96d6e9f24
TarunTomar122 <Tarun@2001>
2020-10-02 11:32:39 +0530

    Fixed my earlier PR with separate example files

---
+++ logistic_example.py
@@ -0,0 +1,17 @@
+from MLlib.models import LogisticRegression
+from MLlib.optimizers import Adam
+from MLlib.loss_func import LogarithmicError
+from MLlib.utils.misc_utils import read_data, printmat
+
+
+X, Y = read_data('MLlib/datasets/logistic_reg_00.txt')
+
+linear_model = LogisticRegression()
+
+optimizer = Adam(0.03, LogarithmicError)
+
+linear_model.fit(X, Y, optimizer=optimizer, epochs=200, zeros=False)
+
+printmat('predictions', linear_model.predict(X))
+
+linear_model.save('test')
