ff89773bf2d9647819ef7d4acad83ea517b64322
Hinal-Srivastava <hinalsri@gmail.com>
2020-10-02 14:04:17 +0530

    Huber Loss Function

--- MLlib/loss_func.py
+++ MLlib/loss_func.py
@@ -26,3 +26,17 @@ class LogarithmicError():
         M = X.shape[0]
         H = sigmoid(np.dot(X, W).T)
         return (1/M)*(np.dot(X.T, (H-Y).T))
+
+class Huber_Loss_Function():
+    @staticmethod
+    def loss(m, b, X, Y, delta):
+        N = len(X)
+        for i in range(N):
+            
+            if abs(Y[i] - m*X[i] - b) <= delta: #Quadratic Derivative for <=delta
+                m += -X[i] * (Y[i] - (m*X[i] + b))
+                b += - (Y[i] - (m*X[i] + b))
+            else:
+                m += delta * X[i] * ((m*X[i] + b) - Y[i]) / abs((m*X[i] + b) - Y[i]) #Linear Derivative for >delta
+                b += delta * ((m*X[i] + b) - Y[i]) / abs((m*X[i] + b) - Y[i])
+        return m, b
