3e7e60da069d18f62ed364df355996c381bf0676
SaiSrichandra <saisrichandra@gmail.com>
2020-10-03 16:18:07 +0530

    Made a few corrections

--- MLlib/activations.py
+++ MLlib/activations.py
@@ -26,11 +26,13 @@ def leakyRelu(X):
     return np.maximum(0.01*X,X)
 
 
-def elu(X):
+def elu(X, alpha = 1.0):
+    assert(alpha > 0)
     output = []
     for x in X:
         if x>0:
             output.append(x)
         else:
-            output.append(1.0*(np.exp(x)-1))
+            output.append(alpha*(np.exp(x)-1))
     return np.array(output)
+
