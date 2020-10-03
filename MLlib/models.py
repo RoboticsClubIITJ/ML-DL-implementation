c92faa0ee86e8a77815d8f2e928ec7d5e09b67f1
hunter <hunterhobbs99@gmail.com>
2020-10-02 19:32:51 -0600

    Added best-weight option to LinearRegression.predict API

--- MLlib/models.py
+++ MLlib/models.py
@@ -13,23 +13,31 @@ class LinearRegression():
     def fit(self, X, Y, optimizer=GradientDescent, epochs=25, zeros=False):
 
         self.weights = generate_weights(X.shape[1], 1, zeros=zeros)
+        self.best_weights = {weights: None, loss: float('inf')}
 
         print("Starting training with loss:",
               optimizer.loss_func.loss(X, Y, self.weights))
         for epoch in range(1, epochs+1):
             print("======================================")
-            self.weights = optimizer.iterate(X, Y, self.weights)
             print("epoch:", epoch)
-            print("Loss in this step: ",
-                  optimizer.loss_func.loss(X, Y, self.weights))
+            self.weights = optimizer.iterate(X, Y, self.weights)
+            epoch_loss = optimizer.loss_func.loss(X, Y, self.weights)
+            if epoch_loss < best_weights['loss']:
+                print("updating best weights (loss: {})".format(epoch_loss))
+                best_weights['weights'] = self.weights
+                best_weights['loss'] = epoch_loss
+            print("Loss in this step: ", epoch_loss)
 
         print("======================================\n")
         print("Finished training with final loss:",
               optimizer.loss_func.loss(X, Y, self.weights))
         print("=====================================================\n")
 
-    def predict(self, X):
-        return np.dot(X, self.weights)
+    def predict(self, X, weights='best'):
+        if weights == best:
+            return np.dot(X, self.best_weights['weights'])
+        elif weights == last:
+            return np.dot(X, self.weights)
 
     def save(self, name):
         with open(name + '.rob', 'ab') as robfile:
