ba9f420fa48f4532f0fc80bf3808eb225d4e6810
Shubham Agrawal <shubham.ag6845@gmail.com>
2020-12-08 17:47:55 +0530

    removed quark and added few tests

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -3,6 +3,7 @@ from MLlib.utils.raises_util import raises
 from MLlib.activations import Sigmoid
 
 def test_Sigmoid():
-    X=np.random.random((3,1))
-    assert 1 / (1 + np.exp(-X)) == Sigmoid.activation(X)
-    assert (1 / (1 + np.exp(-X)))*(1-(1 / (1 + np.exp(-X))))==Sigmoid.derivative(X)
+    X=np.random.random((3,2))
+    print(1 / (1 + np.exp(-X)))
+    assert np.array_equal( (1 / (1 + np.exp(-X))), Sigmoid.activation(X)) is True
+    assert np.array_equal((1 / (1 + np.exp(-X)))*(1-(1 / (1 + np.exp(-X)))),Sigmoid.derivative(X))
