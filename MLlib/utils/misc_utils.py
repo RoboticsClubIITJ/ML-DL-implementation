338a89229da541e4285a3ad22508bfef5effc5e5
Player0109 <apratapsingh.mec18@itbhu.ac.in>
2020-12-08 22:09:48 +0530

    One Hot Encoder
    Implemented One Hot Encoder.

--- MLlib/utils/misc_utils.py
+++ MLlib/utils/misc_utils.py
@@ -71,44 +71,38 @@ def load_model(name):
     return model
 
 
-class OneHotEncoder:
+class OneHotEncoder():
+    """
+    FUNCTIONS
+    (1) FIT(INPUT_X, THRESHOLD)
+    (2) CHECK_TRANSFORM(INPUT_X)
+    (3) TRANSFORM(INPUT_X)
+    (4) FIT_TRANSFORM(INPUT_X, THRESHOLD)
 
-    # FUNCTIONS
-    # (1) FIT(INPUT_X, THRESHOLD) ---
-    #         It is used to calculate the number of unique values in
-    #         each column and tell whether a particular column
-    #         should be encoded or not.
-    # (2) CHECK_TRANSFORM(INPUT_X) ---
-    #                     It is used to check whether the data
-    #                     which is being transformed has same
-    #                     values as the data which was used
-    #                     to fit it.
-    # (3) TRANSFORM(INPUT_X) ---
-    #               It is used to OneHotEncode the data
-    #               based on the data which was used to fit
-    # (4) FIT_TRANSFORM(INPUT_X, THRESHOLD) ---
-    #                   This fuction is just a combination
-    #                   of the fit and the transform fuction.
+    INPUTS
+     X - It is a numpy array of size n x m.
+     thresh - It is a threshold value which is calulated as
+        THRESH = (NUMBER OF UNIQUE VALUES IN A COLUMN)/(LENNGTH OF COLUMN).
+        Column whose threshold value is below the input
+        threshold value which be encode otherwise not.
 
-    # INPUTS
-    # X - It is a numpy array of size n x m.
-    # thresh - It is a threshold value which is calulated as
-    #          THRESH = (NUMBER OF UNIQUE VALUES IN A COLUMN)/
-    #                   (LENGTH OF COLUMN).
-    #          Column whose threshold value is below the
-    #          input threshold value which be encode otherwise not.
-
-    # VARIABLES
-    # ncols - It is used to store the number of columns in the fit data.
-    # arr_dic - Is is an array of dictionary, where each dictionary
-    #           is the LabelEncoded value of a particular column.
-    # arr_nunique - It is an array which  is used to store the number of
-    #               unique values in a particular column.
-    # encode - It is an array of the size of the number of columns in the
-    #          fit data. It has a value of 1 if the columns is to be
-    #          encoded otherwise 0.
+    VARIABLES
+     ncols - It is used to store the number of columns in the fit data.
+     arr_dic - Is is an array of dictionary, where each dictionary
+        is the LabelEncoded value of a particular column.
+     arr_nunique - It is an array which  is used to store
+        the number of unique values in a particular column.
+     encode - It is an array of the size of the number of columns
+        in the fit data.
+        It has a value of 1 if the columns is to be encoded otherwise 0.
+    """
 
     def fit(self, X, thresh):
+        """
+        FIT(INPUT_X, THRESHOLD) --- It is used to calculate the
+            number of unique values in each column and tell
+            whether a particular column should be encoded or not.
+        """
         n = X.shape[0]
         m = X.shape[1]
 
@@ -121,7 +115,7 @@ class OneHotEncoder:
             n_unique = len(ls)
             dic = dict()
 
-            if n_unique / n > thresh:
+            if n_unique/n > thresh:
                 self.encode.append(0)
 
             else:
@@ -133,13 +127,16 @@ class OneHotEncoder:
             self.arr_nunique.append(n_unique)
 
     def check_transform(self, X):
+        """
+        CHECK_TRANSFORM(INPUT_X) --- It is used to check whether the data which
+            is being transformed has same values as the data which was
+            used to fit it.
+        """
         m = X.shape[1]
 
         if self.ncols != m:
-            print(
-                "Number of Columns in input data of fit function and \
-                 transform function are different"
-            )
+            print("Number of Columns in input data of fit function and', \
+                    'transform function are different")
             return False
 
         for i in range(m):
@@ -147,25 +144,22 @@ class OneHotEncoder:
             n_unique = len(ls)
 
             if self.arr_nunique[i] != n_unique:
-                print(
-                    "Mismatch in the number of unique values in the "
-                    + str(i)
-                    + "th column"
-                )
+                print('Mismatch in the number of unique values in',
+                      'the '+str(i)+'th column')
                 return False
             for val in ls:
                 if val not in self.arr_dic[i].keys():
-                    print(
-                        str(i)
-                        + "th column contain a value which was not in \
-                           the data used to fit data"
-                    )
+                    print(str(i)+'th column contain a value which was',
+                          'not in the data used to fit data')
                     return False
 
         return True
 
     def transform(self, X):
-
+        """
+        TRANSFORM(INPUT_X) --- It is used to OneHotEncode the data
+            based on the data which was used to fit
+        """
         check = self.check_transform(X)
         if not check:
             return None
@@ -189,5 +183,9 @@ class OneHotEncoder:
         return self.out_x
 
     def fit_transform(self, X, thresh):
+        """
+        FIT_TRANSFORM(INPUT_X, THRESHOLD) --- This fuction is just a
+            combination of the fit and the transform fuction.
+        """
         self.fit(X, thresh)
         return self.transform(X)
