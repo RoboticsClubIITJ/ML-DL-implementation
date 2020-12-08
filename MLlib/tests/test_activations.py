a1fc203ae3e715bc9fcd5caa7c475f1bb8d3ac11
Shubham Agrawal <shubham.ag6845@gmail.com>
2020-12-08 18:41:13 +0530

    removed flak8 errors

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -1,9 +1,13 @@
 import numpy as np
-from MLlib.utils.raises_util import raises
 from MLlib.activations import Sigmoid
 
+
 def test_Sigmoid():
-    X=np.random.random((3,2))
-    print(1 / (1 + np.exp(-X)))
-    assert np.array_equal( (1 / (1 + np.exp(-X))), Sigmoid.activation(X)) is True
-    assert np.array_equal((1 / (1 + np.exp(-X)))*(1-(1 / (1 + np.exp(-X)))),Sigmoid.derivative(X))
+    X = np.random.random((3, 2))
+    assert np.array_equal(
+        (1 / (1 + np.exp(-X))),
+        Sigmoid.activation(X)
+        ) is True
+    assert np.array_equal(
+        (1 / (1 + np.exp(-X)))*(1-(1 / (1 + np.exp(-X)))),
+        Sigmoid.derivative(X)) is True
