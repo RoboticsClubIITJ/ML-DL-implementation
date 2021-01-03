357fde9641375f31b937b01ca3df6ef3f717b5da
tusharnankani <tusharnankani3@gmail.com>
2021-01-03 12:05:49 +0530

    :hammer: refactor: use 'raise AssertionError'

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -5,29 +5,34 @@ from MLlib.activations import unit_step
 
 def test_Sigmoid():
     X = np.random.random((3, 2))
-    assert np.array_equal(
+    if np.array_equal(
         (1 / (1 + np.exp(-X))),
         Sigmoid.activation(X)
-        ) is True
-    assert np.array_equal(
+        ) is not True:
+        raise AssertionError
+    if np.array_equal(
         (1 / (1 + np.exp(-X)))*(1-(1 / (1 + np.exp(-X)))),
-        Sigmoid.derivative(X)) is True
+        Sigmoid.derivative(X)) is not True:
+        raise AssertionError
 
 
 def test_Relu():
     X = np.array([[1, -2, 3], [-1, 2, 1]])
-    assert np.array_equal(
+    if np.array_equal(
         np.maximum(0, X),
         Relu.activation(X)
-        ) is True
-    assert np.array_equal(
+        ) is not True:
+        raise AssertionError
+    if np.array_equal(
         np.greater(X, 0).astype(int),
-        Relu.derivative(X)) is True
+        Relu.derivative(X)) is not True:
+        raise AssertionError
 
 
 def test_unit_step():
     X = np.array([[1, -2, 3], [-1, 2, 1]])
-    assert np.array_equal(
+    if np.array_equal(
         np.heaviside(X, 1),
         unit_step(X)
-        ) is True
+        ) is not True:
+        raise AssertionError
