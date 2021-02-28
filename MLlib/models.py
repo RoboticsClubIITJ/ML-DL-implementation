6206c4b99b8a93c0b4aa86bf1bd4d0ae3b354fd9
Siddharth-Singh27 <77073857+Siddharth-Singh27@users.noreply.github.com>
2021-02-28 19:59:14 +0530

    include numerical outlier method

--- MLlib/models.py
+++ MLlib/models.py
@@ -978,3 +978,48 @@ class PCA(PCA_utils):
         self.singular_value = singular_value[:n_components]
         self.fitted = True
         return U, S, Vh
+
+#-------------------------------Numerical Outliers Method--------------------------------
+
+class Numerical_outliers: 
+    def get_percentile(c,percentile_rank): 
+        """
+           get_percentile Function 
+
+           PARAMETER
+           =========
+           c:ndarray(dtype=float,ndim=1)
+             input dataset
+           percentile_rank: float type
+           
+           RETURNS
+           =======
+           Data corresponding to percentile rank
+           """
+        
+        
+        d=np.sort(c)
+        index=int(((len(d)-1)*percentile_rank)//100)
+        return d[index]
+    
+    def get_outliers(x):
+        
+        """ get_outliers Function
+    
+           PARAMETER
+           =========
+    
+        x:ndarray(dtype=float,ndim=1)
+            input dataset
+         """
+    
+        d=np.sort(x)
+        e=np.median(d)
+        Q1= Numerical_outliers.get_percentile(x,25)
+        Q3= Numerical_outliers.get_percentile(x,75)
+        iqr=Q3-Q1
+        lowerbound=Q1-1.5*iqr
+        upperbound=Q3+1.5*iqr
+        for i in range(len(x)):
+             if x[i]>upperbound or x[i]<lowerbound:
+                print("outlier=",x[i])
