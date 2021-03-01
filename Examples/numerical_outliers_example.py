2de3071c60cef740303f826f54e0b52abcbcdf80
Siddharth-Singh27 <77073857+Siddharth-Singh27@users.noreply.github.com>
2021-03-01 14:54:14 +0530

    Pep8 errors solved

--- Examples/numerical_outliers_example.py
+++ Examples/numerical_outliers_example.py
@@ -1,7 +1,7 @@
 
-from MLlib.utils.misc_utils import read_data, printmat
+from MLlib.utils.misc_utils import read_data
 from MLlib.models import Numerical_outliers
 
-x,y=read_data("datasets/numerical_outliers.txt")
+x, y = read_data("datasets/numerical_outliers.txt")
 
 Numerical_outliers.get_outliers(y[0])
