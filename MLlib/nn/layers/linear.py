bf0a838e76f9916e5920fbfe7700e7464d3212e9
Nakul Sharma <nakul120.ns@gmail.com>
2021-03-12 23:41:38 +0530

    updated Linear layer to incorporate activation function

--- MLlib/nn/layers/linear.py
+++ MLlib/nn/layers/linear.py
@@ -3,18 +3,28 @@ import MLlib.nn as nn
 
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features):
+
+    __slots__ = ('weights', 'bias', 'actvn_fn')
+
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 activation_fn=None):
+
         super().__init__()
-        self.bias = MLlib.Tensor(0.,
-                                 requires_grad=True,
-                                 is_parameter=True)
+        self.bias = MLlib.Tensor.randn(out_features,
+                                       requires_grad=True,
+                                       is_parameter=True)
 
-        self.weight = MLlib.Tensor.randn(out_features,
-                                         in_features,
-                                         is_parameter=True)
+        self.weights = MLlib.Tensor.randn(out_features,
+                                          in_features,
+                                          requires_grad=True,
+                                          is_parameter=True)
 
-        self.weight.requires_grad = True
+        self.actvn_fn = activation_fn() if activation_fn is not None else None
 
     def forward(self, x):
-        x = (x @ self.weight.T) + self.bias
+        x = (x @ self.weights.T) + self.bias
+        if self.actvn_fn is not None:
+            x = self.actvn_fn.apply(x)
         return x
