0ff4b383669af8a9dbe9bf4f1ad8160b81a55c02
Kwanit Gupta <gupta.45@iitj.ac.in>
2021-03-22 02:12:03 +0530

    Update preprocess_example.py

--- Examples/preprocess_example.py
+++ Examples/preprocess_example.py
@@ -11,4 +11,3 @@ print(Scale.Feature_Clipping(100000, 10000))
 print(Scale.Mean_Normalization())
 print(Scale.MinMax_Normalization(new_min=0, new_max=1))
 Scale.Bell_curve('datasets/salaryinp.csv', 'Salary')
-
