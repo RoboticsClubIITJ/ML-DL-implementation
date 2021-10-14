763d9660346369fcaa94fb0d8ad8b506c8dc4381
Devyani Gorkar <gorkar.1@iitj.ac.in>
2021-10-14 17:16:25 +0530

    Graph of K means cluster
    Plotted a graph which shows different cluster formed and their cluster centers.

--- Examples/k_means_clustering_example.py
+++ Examples/k_means_clustering_example.py
@@ -5,4 +5,18 @@ X = np.genfromtxt('datasets/k_means_clustering.txt')
 
 KMC = KMeansClustering()
 
-KMC.work(X, 3, 7)
+na,ca=KMC.work(X, 3, 7,config=True)
+KMC.plot(X,3,7 )
+
+
+        
+
+
+
+      
+        
+
+
+
+        
+    
