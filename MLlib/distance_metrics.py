504892f1a8f4aa05ec4a621566dc7422710c8763
ShubhamMishra1611 <mishra.39@iitj.ac.in>
2022-10-07 14:28:11 +0530

    Updated features in README.md and code formatting

--- MLlib/distance_metrics.py
+++ MLlib/distance_metrics.py
@@ -1,13 +1,15 @@
 import numpy as np
+
+
 class Distance_metrics:
     """ 
     Calculate distance between each corresponding points
     of two arrays using different distance metrics
     """
-    def Euclidean_Distance(X1,X2):
+    def Euclidean_Distance(X1, X2):
         """"
         Returns the list of euclidean distance
-        between two corresponding points of 
+        between two corresponding points of
         two arrays
 
         PARAMETERS
@@ -23,17 +25,17 @@ class Distance_metrics:
 
         distance:list
         Returns the list of euclidean distance
-        between two corresponding points of 
+        between two corresponding points of
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum((X1[i]-X2[i])**2)
+            single = 0
+            single = np.sum((X1[i]-X2[i])**2)
             distance.append(np.sqrt(single))
-        return(distance)
+        return (distance)
 
-    def Manhattan_Distance(X1,X2):
+    def Manhattan_Distance(X1, X2):
         """"
         Returns the list of manhattan distance
         between two corresponding points of
@@ -52,17 +54,17 @@ class Distance_metrics:
 
         distance:list
         Returns the list of manhattan distance
-        between two corresponding points of 
+        between two corresponding points of
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum(abs(X1[i]-X2[i]))
+            single = 0
+            single = np.sum(abs(X1[i]-X2[i]))
             distance.append(single)
-        return(distance)
+        return (distance)
 
-    def Chebyshev_Distance(X1,X2):
+    def Chebyshev_Distance(X1, X2):
         """"
         Returns the list of chebyshev distance
         between two corresponding points of 
@@ -84,14 +86,14 @@ class Distance_metrics:
         between two corresponding points of 
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum(max(X1[i]-X2[i]))
+            single = 0
+            single = np.sum(max(X1[i]-X2[i]))
             distance.append(single)
-        return(distance)
+        return (distance)
 
-    def Minkowski_Distance(X1,X2,p):
+    def Minkowski_Distance(X1, X2, p):
         """"
         Returns list of minkowski distance of order 'p'
         between two corresponding vectors of
@@ -116,14 +118,14 @@ class Distance_metrics:
         between two corresponding vectors of 
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum((abs(X1[i]-X2[i]))**p)
+            single = 0
+            single = np.sum((abs(X1[i]-X2[i]))**p)
             distance.append((single)**(1/p))
-        return(distance)
-    
-    def WMinkowski_Distance(X1,X2,p,W):
+        return (distance)
+
+    def WMinkowski_Distance(X1, X2, p, W):
         """"
         Returns list of weighted minkowski distance of order 'p'
         between two corresponding vectors weighted by W of
@@ -151,14 +153,14 @@ class Distance_metrics:
         between two corresponding vectors of 
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum((abs(W*(X1[i]-X2[i])))**p)
+            single = 0
+            single = np.sum((abs(W*(X1[i]-X2[i])))**p)
             distance.append((single)**(1/p))
-        return(distance)
+        return (distance)
 
-    def Hamming_Distance(X1,X2):
+    def Hamming_Distance(X1, X2):
         """
         Returns the Hamming distance between
         two binary arrays
@@ -178,12 +180,12 @@ class Distance_metrics:
         two binary arrays
         """
         s = 0
-        for e1,e2 in zip(X1,X2):
+        for e1, e2 in zip(X1, X2):
             s += abs(e1-e2)
         distance = s/len(X1)
         return distance
 
-    def sEuclidean_distance(X1,X2,V):
+    def sEuclidean_distance(X1, X2, V):
         """
         Returns the list of standardized euclidean distance
         between two corresponding points of 
@@ -208,14 +210,14 @@ class Distance_metrics:
         between two corresponding points of 
         two arrays
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            single=0
-            single=np.sum(((X1[i]-X2[i])/V[i])**2)
+            single = 0
+            single = np.sum(((X1[i]-X2[i])/V[i])**2)
             distance.append(np.sqrt(single))
-        return(distance)
+        return (distance)
 
-    def Mahalanobis_Distance(X,d,V=None):
+    def Mahalanobis_Distance(X, d, V=None):
         """
         Returns the mahalanobis distance between 
         points and distribution
@@ -242,17 +244,17 @@ class Distance_metrics:
         Returns the list of mahalanobis distance
         between points and given distribution
         """
-        distance=[]
+        distance = []
         for i in range(len(X)):
-            x_minus_mu = X[i]-np.mean(d,axis=0)
-            if V==None:
-                V=np.cov(d.T)
+            x_minus_mu = X[i]-np.mean(d, axis=0)
+            if V is None:
+                V = np.cov(d.T)
             VI = np.linalg.inv(V)
-            d = np.sqrt(np.dot(np.dot(x_minus_mu,VI),x_minus_mu.T))
+            d = np.sqrt(np.dot(np.dot(x_minus_mu, VI), x_minus_mu.T))
             distance.append(d)
-        return(distance)
-    
-    def __boolean_opr(self,X1,X2):
+        return (distance)
+
+    def __boolean_opr(self, X1, X2):
         """
         Returns result of some bianry operation
         between two arrays.Any non zero value is 
@@ -276,27 +278,26 @@ class Distance_metrics:
         result[3]:Number of dims in which the first value is False and second is True
         result[4]:Number of dims in which both values are False
         """
-        if len(X1)!=len(X2):
+        if len(X1) != len(X2):
             raise TypeError("X1 and X2 must have same length")
-        result=[]
+        result = []
         for i in range(len(X1)):
-            if X1[i]!=0:
-                X1[i]=1
-            if X2[i]!=0:
-                X2[i]=1
+            if X1[i] != 0:
+                X1[i] = 1
+            if X2[i] != 0:
+                X2[i] = 1
         result.append(len(X1))
-        result.append(np.sum((X1==1)&(X2==1)))
-        result.append(np.sum((X1==1)&(X2==0)))
-        result.append(np.sum((X1==0)&(X2==1)))
-        result.append(np.sum((X1==0)&(X2==0)))
-        return(tuple(result))
+        result.append(np.sum((X1 == 1) & (X2 == 1)))
+        result.append(np.sum((X1 == 1) & (X2 == 0)))
+        result.append(np.sum((X1 == 0) & (X2 == 1)))
+        result.append(np.sum((X1 == 0) & (X2 == 0)))
+        return (tuple(result))
 
-
-    def Jaccard_Distance(self,X1,X2):
+    def Jaccard_Distance(self, X1, X2):
         """
         Returns the list of Jaccard distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -310,17 +311,18 @@ class Distance_metrics:
         distance:list
         Returns the list of Jaccard distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append((result[2]+result[3])/(result[1]+result[2]+result[3]))
-        return(distance)        
-    
-    def Matching_Distance(self,X1,X2):
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append((result[2]+result[3]) /
+                            (result[1]+result[2]+result[3]))
+        return (distance)
+
+    def Matching_Distance(self, X1, X2):
         """
         Returns the list of Matching distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -334,17 +336,17 @@ class Distance_metrics:
         distance:list
         Returns the list of Matching distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
+            result = self.__boolean_opr(X1[i], X2[i])
             distance.append((result[2]+result[3])/result[0])
-        return(distance)
+        return (distance)
 
-    def Dice_Distance(self,X1,X2):
+    def Dice_Distance(self, X1, X2):
         """
         Returns the list of Dice distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -358,17 +360,18 @@ class Distance_metrics:
         distance:list
         Returns the list of Dice distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append((result[2]+result[3])/(2*result[1]+result[2]+result[3]))
-        return(distance)
-    
-    def Kulsinki_Distance(self,X1,X2):
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append((result[2]+result[3]) /
+                            (2*result[1]+result[2]+result[3]))
+        return (distance)
+
+    def Kulsinki_Distance(self, X1, X2):
         """
         Returns the list of Kulsinki distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -382,17 +385,18 @@ class Distance_metrics:
         distance:list
         Returns the list of Kulsinki distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append((result[2]+result[3]+result[0]-result[1])/(result[2]+result[3]+result[0]))
-        return(distance)
-    
-    def Rogers_Tanimoto_Distance(self,X1,X2):
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append(
+                (result[2]+result[3]+result[0]-result[1])/(result[2]+result[3]+result[0]))
+        return (distance)
+
+    def Rogers_Tanimoto_Distance(self, X1, X2):
         """
         Returns the list of Rogers-Tanimoto distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -406,17 +410,18 @@ class Distance_metrics:
         distance:list
         Returns the list of Rogers-Tanimoto distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append(2*(result[2]+result[3])/(result[2]+result[3]+result[0]))
-        return(distance)
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append(2*(result[2]+result[3]) /
+                            (result[2]+result[3]+result[0]))
+        return (distance)
 
-    def Russell_Rao_Distance(self,X1,X2):
+    def Russell_Rao_Distance(self, X1, X2):
         """
         Returns the list of Russell-Rao distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -430,17 +435,17 @@ class Distance_metrics:
         distance:list
         Returns the list of Russell-Rao distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
+            result = self.__boolean_opr(X1[i], X2[i])
             distance.append((result[0]-result[1])/result[0])
-        return(distance)
+        return (distance)
 
-    def Sokal_Michener_Distance(self,X1,X2):
+    def Sokal_Michener_Distance(self, X1, X2):
         """
         Returns the list of Sokal-Michener distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -454,17 +459,18 @@ class Distance_metrics:
         distance:list
         Returns the list of Sokal-Michener distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append(2*(result[2]+result[3])/(result[0]+result[2]+result[3]))
-        return(distance)
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append(2*(result[2]+result[3]) /
+                            (result[0]+result[2]+result[3]))
+        return (distance)
 
-    def Sokal_Sneath_Distance(self,X1,X2):
+    def Sokal_Sneath_Distance(self, X1, X2):
         """
         Returns the list of Sokal-Sneath distance between
         two corresponding vectors of two binary arrays
-        
+
         PARAMETERS
         ==========
         X1:ndarray(dtype=int,axis=1)
@@ -478,8 +484,9 @@ class Distance_metrics:
         distance:list
         Returns the list of Sokal-Sneath distance
         """
-        distance=[]
+        distance = []
         for i in range(len(X1)):
-            result=self.__boolean_opr(X1[i],X2[i])
-            distance.append((result[2]+result[3])/(0.5*result[1]+result[2]+result[3]))
-        return(distance)
+            result = self.__boolean_opr(X1[i], X2[i])
+            distance.append((result[2]+result[3]) /
+                            (0.5*result[1]+result[2]+result[3]))
+        return (distance)
