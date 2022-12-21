a35c733335988b6c08740730b841e85ef24cef62
Jason Daniel <jason-daniel@Jasons-MacBook-Air.local>
2022-12-21 17:27:15 +0530

    Unit test for TanH added

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -1,6 +1,12 @@
-from MLlib.activations import Sigmoid
+from MLlib.activations import Sigmoid, TanH
 
 def test_sigmoid():
     X = 0
     assert Sigmoid.activation(X) == 0.5
     assert Sigmoid.derivative(X) == 0.25
+
+
+def test_tanh():
+    X = 0
+    assert TanH.activation(X) == 0
+    assert TanH.derivative(X) == 1
