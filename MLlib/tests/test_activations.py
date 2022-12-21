5c2c7a3e34300c10c74a2a5268e16ebdff168a60
Somshuvra Basu <93534229+SomshuvraBasu@users.noreply.github.com>
2022-12-21 22:47:54 +0530

    Wrote the test for ReLU Function

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -1,4 +1,6 @@
-from MLlib.activations import Sigmoid, TanH
+from MLlib.activations import Sigmoid, TanH, Relu
+import numpy as np
+import pytest
 
 def test_sigmoid():
     X = 0
@@ -10,3 +12,22 @@ def test_tanh():
     X = 0
     assert TanH.activation(X) == 0
     assert TanH.derivative(X) == 1
+
+@pytest.mark.parametrize("X", [np.array([1,2,3,4,5]),np.array([-1,-2,-3,-4,-5]),np.array([0,0,0,0,0])])
+def test_Relu(X):
+    Y=np.zeros(len(X))
+    dY=np.zeros(len(X))
+    for i in range(len(X)):
+        if X[i]>0:
+            Y[i]=X[i]
+        else:
+            Y[i]=0
+
+    for i in range(len(X)):
+        if X[i]>0:
+            dY[i]=1
+        else:
+            dY[i]=0
+
+    assert(np.array_equal(Relu.activation(X),Y))
+    assert(np.array_equal(Relu.derivative(X),dY))
