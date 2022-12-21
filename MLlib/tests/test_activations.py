0e3cb87054d3540133ebba06c3042cdeb7723692
PT-10 <102211549+PT-10@users.noreply.github.com>
2022-12-21 17:28:36 +0530

    Adds Softmax unit test to test_demo.py

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -1,6 +1,11 @@
-from MLlib.activations import Sigmoid
+from MLlib.activations import Sigmoid, Softmax
 
 def test_sigmoid():
     X = 0
     assert Sigmoid.activation(X) == 0.5
     assert Sigmoid.derivative(X) == 0.25
+
+def test_Softmax():
+    X = 0
+    assert Softmax.activation(X) == 1
+    assert Softmax.derivative(X) == 1.5    
