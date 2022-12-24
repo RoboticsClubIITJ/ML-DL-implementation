382e0e483fd2cfceb71624a72a09e7336a03ff6b
Rohan Singh <54505869+rohansingh9001@users.noreply.github.com>
2022-12-24 21:21:26 +0530

    Corrected formatting errors

--- MLlib/tests/test_activations.py
+++ MLlib/tests/test_activations.py
@@ -2,7 +2,6 @@ import numpy as np
 from MLlib.activations import Sigmoid, TanH, Softmax
 
 
-
 def test_sigmoid():
     X = 0
     assert Sigmoid.activation(X) == 0.5
@@ -13,9 +12,9 @@ def test_tanh():
     X = 0
     assert TanH.activation(X) == 0
     assert TanH.derivative(X) == 1
-    
-    
+
+
 def test_Softmax():
     X = np.array([0])
-    assert Softmax.activation(X) ==  np.array([1,2,3,4]).all()
-    assert Softmax.derivative(X) == np.array([0,1,2,3,4,5]).all()
+    assert Softmax.activation(X) ==  np.array([1, 2, 3, 4]).all()
+    assert Softmax.derivative(X) == np.array([0, 1, 2, 3, 4, 5]).all()
